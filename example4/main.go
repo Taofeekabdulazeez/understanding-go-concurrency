bd2f09be9c6aa6ef9fa7ad4786ca60a5c806adbd
Taofeekabdulazeez <taofeekabdulazeez2020@gmail.com>
2025-11-14 21:58:52 +0100

    refactor: simplify HTTP client usage and response handling in anagram solver example

--- example4/main.go
+++ example4/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
-	"time"
 )
 
 type APIResponse struct {
@@ -14,14 +12,11 @@ type APIResponse struct {
 }
 
 func main() {
-	ctx := context.Background()
-	client := &http.Client{
-		Timeout: 5 * time.Second,
-	}
+	client := &http.Client{}
 
 	api_url := "https://anagram-solver.onrender.com/master"
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, api_url, nil)
+	req, err := http.NewRequest(http.MethodGet, api_url, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -39,12 +34,5 @@ func main() {
 		panic(err)
 	}
 
-	println("Word:", data.Word)
-	println("Results:", data.Results)
-	println("Anagrams:")
-
-	for _, anagram := range data.Anagrams {
-		println("-", anagram)
-	}
-
+	println("Data:", data)
 }
